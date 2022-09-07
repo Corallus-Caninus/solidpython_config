7b45fcf30f885ad97bc192deb2c5f74fbd3d6716
Corallus-Caninus <ward.joshua92@yahoo.com>
2022-09-07 13:48:01 -0700

    rm squash

--- src/bin/gen_man.rs
+++
@@ -1,8 +0,0 @@
-// create a man page for solidpython_config using the man crate
-use man::prelude::*;
-fn main(){
-    let page = Manual::new("solidpython_config").about("a crate that generates configuration based solidpython template")
-    .author(Author::new("Josh Ward"))
-    .render();
-    println!("{}",page);
-}
